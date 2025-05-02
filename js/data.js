21f348cb6a79fe3409225b1977ad93d388fe6e0e
iiivanmarko <ivan.marko@werkstatt54.com>
2025-05-02 21:49:59 +0200

    just loging data

--- js/data.js
+++ js/data.js
@@ -9,9 +9,7 @@ export class DATA {
   async loaddata() {
     if (localStorage.getItem("data")) {
       this.data = JSON.parse(localStorage.getItem("data"));
-      this.data.forEach(e => {
-        FLOWERS.get(e.id).grow()
-      });
+  
       return;
     }
 
@@ -19,28 +17,36 @@ export class DATA {
       owner: person,
     })
       .done(async (ret) => {
+        
         const parsedData = JSON.parse(ret);
+        
         window.localStorage.setItem("data", JSON.stringify(parsedData));
         this.data = parsedData;
       })
       .fail(function (error) {
         
       });
-  }
-
-  addItem(text, owner) {
-    const item = {
-      id: 0,
-      wunsch: text,
-      time: new Date().getTime(),
-      owner: owner,
-    };
-    FLOWERS.get(0).grow()
-    this.data.push(item);
-  }
-
-  renderData() {
+    }
     
+    addItem(text, owner) {
+      const item = {
+        id: 0,
+        wunsch: text,
+        time: new Date().getTime(),
+        owner: owner,
+      };
+      FLOWERS.get(0).grow()
+      this.data.push(item);
+    }
+    
+    renderData() {
+
+      console.log(this.data);
+      
+      this.data.forEach(e => {
+        FLOWERS.get(e.id).grow()
+      });
+      
   }
 
   saveData() {
@@ -56,6 +62,8 @@ export class DATA {
     })
       .done(async () => {
         await this.loaddata();
+
+        
         this.renderData();
       })
       .fail(function (error) {
